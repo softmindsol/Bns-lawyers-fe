77be6d9567d5d39c62a98dcf8292fcb2fa0d2298
Muhammad Ahsan <ahsanwebengr@gmail.com>
2025-02-14 18:48:34 +0500

    fix: loader size

--- src/components/loader/index.jsx
+++ src/components/loader/index.jsx
@@ -1,7 +1,7 @@
 const Loader = () => {
   return (
     <div className="flex h-screen items-center justify-center bg-white">
-      <div className="animate-spin rounded-full border-4 border-solid border-primary border-t-transparent"></div>
+      <div className="size-10 animate-spin rounded-full border-4 border-solid border-primary border-t-transparent"></div>
     </div>
   );
 };
